a819f13ddf8e7cda782b60e3ac00e6766eea4de4
kiki-doyouloveme <2858785543@qq.com>
2024-09-10 19:16:54 +0800

    id

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ pool = PooledDB(
     host='localhost',
     port=3306,
     user='root',
-    passwd='Pyh019026',
+    passwd='',
     database='binance_demo',
     charset='utf8',
     # 让查询结果是一个 dict
